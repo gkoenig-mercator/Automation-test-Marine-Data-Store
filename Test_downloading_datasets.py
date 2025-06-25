18eb76ff47721beb8c58a5592f4fb4f4dc71b379
gkoenig <gkoenig@mercator-ocean.fr>
2025-06-25 11:30:36 +0000

    Changes error handling and download zone for Europe

--- Test_downloading_datasets.py
+++ Test_downloading_datasets.py
@@ -15,29 +15,29 @@ region_identifier = {'Mediterranean': {'keywords': ['MEDSEA', 'MED_SST', 'MED_PH
                                        'min_lat':42.5,
                                        'max_lat':44},
                      'Antarctic': {'keywords': ['ANT_PHY','antarctic','_ant_'],
-                                       'min_lon':-48,
-                                       'max_lon': -40,
+                                       'min_lon':-44,
+                                       'max_lon': -43,
                                        'min_lat':-90,
                                        'max_lat':-60},
                      'Arctic': {'keywords': ['ARCTIC', 'ARC_BGC', 'SEA_ICE_GLO','SEAICE_ARC', 'INSITU_ARC','_arc_','arctic'],
-                                       'min_lon':-48,
-                                       'max_lon': -40,
-                                       'min_lat':68,
-                                       'max_lat':73},
+                                       'min_lon':-45,
+                                       'max_lon': -44,
+                                       'min_lat':69,
+                                       'max_lat':70},
                      'Iberia-Biscay-Ireland': {'keywords': ['IBI','_ibi_'],
-                                       'min_lon':-5.5,
+                                       'min_lon':-4.5,
                                        'max_lon': -3,
                                        'min_lat':44,
-                                       'max_lat':47},
+                                       'max_lat':45},
                      'NorthWestern European Shelf': {'keywords': ['NWSHELF', 'NWS', 'NORTHWESTSHELF','_nwshelf_'],
-                                       'min_lon':2.5,
-                                       'max_lon': 5,
-                                       'min_lat':53,
-                                       'max_lat':57},
+                                       'min_lon':3,
+                                       'max_lon': 4,
+                                       'min_lat':54,
+                                       'max_lat':56},
                      'Atlantic North': {'keywords': ['ATL','_atl_'],
-                                       'min_lon':-16,
-                                       'max_lon': -12,
-                                       'min_lat':50,
+                                       'min_lon':-14,
+                                       'max_lon': -13,
+                                       'min_lat':53,
                                        'max_lat':56},
                      'Baltic': {'keywords': ['BALTICSEA', 'BAL_BGC','SST_BAL', 'SEAICE_BAL','INSITU_BAL','BALTIC','_bal_'],
                                        'min_lon':18,
@@ -50,15 +50,15 @@ region_identifier = {'Mediterranean': {'keywords': ['MEDSEA', 'MED_SST', 'MED_PH
                                        'min_lat':61,
                                        'max_lat':62.5},
                     'Europe': {'keywords': ['_eur_'],
-                                       'min_lon':0,
-                                       'max_lon': 10.,
-                                       'min_lat':34.,
-                                       'max_lat':40.},
+                                       'min_lon':5,
+                                       'max_lon': 6.,
+                                       'min_lat':36.,
+                                       'max_lat':37.},
                     'Global': {'keywords': ['_glo_'],
-                                       'min_lon':-38,
+                                       'min_lon':-35,
                                        'max_lon': -34,
-                                       'min_lat':-3,
-                                       'max_lat': 3}                           
+                                       'min_lat':-1,
+                                       'max_lat': 1}                           
                                                 }
 
 
@@ -112,6 +112,10 @@ downloadable = []
 last_downloadable_time = []
 regions = []
 
+first_error = []
+second_error = []
+third_error = []
+
 dataset_informations = pd.read_csv(os.path.join(args.data_dir, 'list_of_informations_from_the_describe.csv'))
 
 for index, dataset_information in dataset_informations.iterrows():
@@ -129,18 +133,27 @@ for index, dataset_information in dataset_informations.iterrows():
             downloadable.append(True)
             last_downloadable_time.append(dataset_information['last_available_time'])
             os.remove('data/test.nc')
+
+            first_error.append(None)
+            second_error.append(None)
+            third_error.append(None)
         except Exception as e:
             exception_message = str(e)
             print("There was an error", exception_message)
+            first_error.append(exception_message)
             try :
                 subset_status = download_in_given_time_period(dataset_information)
                 print(subset_status.message, subset_status.status)
                 downloadable.append(True)
                 last_downloadable_time.append(dataset_information['last_available_time'])
                 os.remove('data/test.nc')
+                second_error.append(None)
+                third_error.append(None)
             except Exception as e:
                 exception_message = str(e)
                 print("There was a second error", exception_message)
+
+                second_error.append(None)
                 try: 
                     subset_status = download_whole_dataset(dataset_information)
                     print(subset_status.message, subset_status.status)
@@ -148,17 +161,26 @@ for index, dataset_information in dataset_informations.iterrows():
                     last_downloadable_time.append(dataset_information['last_available_time'])
                     os.remove('data/test.nc')
 
+                    third_error.append(None)
                 except Exception as e:
                     print("There was a third error", exception_message)
                     downloadable.append(False)
                     last_downloadable_time.append(pd.NaT)
+                    third_error.append(str(e))
                     
     else:
         downloadable.append(False)
         last_downloadable_time.append(pd.NaT)
 
+        first_error.append('No last_downloadable_time available')
+        second_error.append(None)
+        third_error.append(None)
+
 dataset_informations['last_downloadable_time'] = last_downloadable_time
 dataset_informations['downloadable'] = downloadable
+dataset_informations['first_error'] = first_error
+dataset_informations['second_error'] = second_error
+dataset_informations['third_error'] = third_error
 
 dataset_informations.to_csv(os.path.join(args.data_dir, 'downloaded_datasets.csv'), index=True)
 
