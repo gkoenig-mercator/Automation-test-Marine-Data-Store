3d807a0b153c96ec7e8dcbf50da123e99aaa28b3
gkoenig <gkoenig@mercator-ocean.fr>
2025-08-20 14:57:38 +0000

    Adds a function that checks if there were any dataset that was not downloadable

---
+++ check_if_download_errors.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+from utils.general import get_data_directory_from_command_line
+
+def no_error_in_download(data_dir):
+    csv_path = os.path.join(data_dir, 'downloaded_datasets.csv')
+    downloaded_datasets_df = pd.read_csv(csv_path)
+
+    return (downloaded_datasets_df["downloadable"]).all()
+
+if __name__ == "__main__":
+    data_dir = get_data_directory_from_command_line()
+    print(no_error_in_download(data_dir))
--- retrieve_from_describe_test_availability_and_push_in_db.py
+++ retrieve_from_describe_test_availability_and_push_in_db.py
@@ -2,6 +2,7 @@ from Test_downloading_datasets import test_dataset_availability_and_save_it
 from extract_datasets_from_describe import collect_and_store_dataset_informations
 from Add_data_in_database import append_data_in_db
 from Set_environment_variables import set_env_var
+from check_if_download_errors import no_error_in_download
 from utils.general import get_data_directory_from_command_line
 import copernicusmarine
 import os
@@ -17,6 +18,7 @@ def main():
     collect_and_store_dataset_informations(data_dir)
     test_dataset_availability_and_save_it(data_dir)
     append_data_in_db(data_dir)
+    print(no_error_in_download(data_dir))
 
 if __name__ == "__main__":
     main()
