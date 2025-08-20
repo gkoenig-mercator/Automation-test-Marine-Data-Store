df6df8dcdf383bbeb05900d03810294e8b2b1244
gkoenig <gkoenig@mercator-ocean.fr>
2025-08-20 09:01:54 +0000

    Sets a .env file for the database informations and modifies the script Add_data_in_database to get the data from this .env file

--- Add_data_in_database.py
+++ Add_data_in_database.py
@@ -3,15 +3,20 @@ import argparse
 from sqlalchemy import create_engine
 import os
 import uuid
+from dotenv import load_dotenv
 from utils.general import get_data_directory_from_command_line
 
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
 username = os.environ["DATABASE_USERNAME"]
 password = os.environ["DATABASE_PASSWORD"]
-database_url = os.environ.get("DATABASE_URL", "postgresql-238316.project-test-datasets-subsetting-toolbox")
-database_name = os.environ.get("DATABASE_NAME","defaultdb")
+database_url = os.environ["DATABASE_URL"]
+database_name = os.environ["DATABASE_NAME"]
+database_port = os.environ["DATABASE_PORT"]
 
 table_name = "List_of_datasets"
-engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{database_url}:5432/{database_name}')
+engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{database_url}:{database_port}/{database_name}')
 
 def append_data_in_db(data_dir): 
 
