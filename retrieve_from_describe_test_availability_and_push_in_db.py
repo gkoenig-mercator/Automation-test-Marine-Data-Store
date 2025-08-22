1364df34eecb2cd90d5c428442ff74391862c110
gkoenig <gkoenig@mercator-ocean.fr>
2025-08-22 09:04:39 +0000

    Remove a script for obtaining the environment variables that is replaced by the package dotenv

--- retrieve_from_describe_test_availability_and_push_in_db.py
+++ retrieve_from_describe_test_availability_and_push_in_db.py
@@ -1,7 +1,6 @@
 from Test_downloading_datasets import test_dataset_availability_and_save_it
 from extract_datasets_from_describe import collect_and_store_dataset_informations
 from Add_data_in_database import append_data_in_db
-from Set_environment_variables import set_env_var
 from check_if_download_errors import no_error_in_download
 from utils.general import get_data_directory_from_command_line
 import copernicusmarine
@@ -11,7 +10,6 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
     data_dir = get_data_directory_from_command_line()
-    set_env_var()
     copernicusmarine.login(username = os.environ["COPERNICUS_MARINE_USERNAME"],
                            password = os.environ["COPERNICUS_SERVICE_PASSWORD"],
                            force_overwrite = True)
