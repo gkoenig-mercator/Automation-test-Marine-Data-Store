d074fc168fdbb9df3cad4e36d60990052d5abeaf
gkoenig <gkoenig@mercator-ocean.fr>
2026-04-16 13:52:30 +0000

    Changes names of Python list for saving testing data due to a modification lost in the merging process

--- script_get_testing.py
+++ script_get_testing.py
@@ -123,8 +123,8 @@ def do_download(base_info, filename):
 
 def test_get_capabilities(max_products: Optional[int] = None):
     datasets_copernicus = copernicusmarine.describe()
-    dataset_informations_dry_run = []
-    dataset_informations_download = []
+    dry_run_records = []
+    download_records = []
 
     for product in datasets_copernicus.products[:max_products] if max_products else datasets_copernicus.products:
         if product.product_id in EXCLUDED_PRODUCTS:
