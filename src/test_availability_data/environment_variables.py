ef244ecbd3778ca6cedc990e8990a727096d29ca
renaudjester <renaud.jester@outlook.fr>
2026-05-13 17:44:39 +0200

    fix: strip env var

--- src/test_availability_data/environment_variables.py
+++ src/test_availability_data/environment_variables.py
@@ -22,7 +22,7 @@ _ENV_VARS_THAT_CAN_BE_EMPTY = {
 
 def __getattr__(name: str) -> str:
     if name in _ENV_VARS:
-        value = os.getenv(name, "")
+        value = os.getenv(name, "").strip().strip("'\"")
         if not value and name not in _ENV_VARS_THAT_CAN_BE_EMPTY:
             raise ValueError(f"{name} environment variable is not set.")
         elif not value:
